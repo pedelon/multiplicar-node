f4a931c0fa63150a3e98d21e589f82a7ab716636
PedroD <pedelon@gmail.com>
2020-06-14 19:09:39 -0400

    updates

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ const colors = require('colors');
 
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar.js')
 
-// let base = 5;
-console.log('argv: ', argv);
-// console.log('base: ', argv.base);
-// console.log('limite: ', argv.limite);
+console.log('argumentos argv: ', argv);
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'listar':
-        // console.log('listar');
         listarTabla(argv.base, argv.limite);
         break;
     case 'crear':
-        console.log('crear');
         crearArchivo(argv.base, argv.limite)
             .then(archivo => console.log(`Archivo creado: ${ archivo }`.green))
             .catch(err => console.log(err));
@@ -25,8 +20,6 @@ switch (comando) {
     default:
         break;
 }
-
-
 
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
